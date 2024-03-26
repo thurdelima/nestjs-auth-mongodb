2e0e3dfe6a39cc7cdb05f6e49dee86c89750a989
Arthur <thurdelima@gmail.com>
2024-03-26 00:37:25 -0300

    feat: import auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/auth_nestjs'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/auth_nestjs'), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
