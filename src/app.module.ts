d044a7032865f51608c0f8da47f6a4f1e82429dc
Arthur <thurdelima@gmail.com>
2024-04-02 01:27:40 -0300

    feat: add productModule in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ProductModule } from './modules/product/product.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/auth_nestjs'), UsersModule, AuthModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/auth_nestjs'), UsersModule, AuthModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
