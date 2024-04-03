f1f67f03dde4de1084b7ad9e3c4957bfb8df0b59
Arthur <thurdelima@gmail.com>
2024-04-03 02:25:13 -0300

    feat: use role in product controller

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateProductUseCase } from './use-cases/create-product.usecase';
 import { CreateProductDTO } from './product.dto';
+import { Role, Roles } from 'src/decorators/roles.decorators';
 
 
 @Controller('products')
@@ -8,6 +9,7 @@ export class ProductController {
   constructor(private readonly createProductUseCase: CreateProductUseCase) {}
 
 
+  @Roles(Role.ADMIN)
   @Post('/')
   async create(@Body() data: CreateProductDTO) {
 
