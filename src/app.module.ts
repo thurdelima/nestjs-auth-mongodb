e2b17b718612f6efa96332caee5daa94bad995d8
Arthur <thurdelima@gmail.com>
2024-04-03 02:23:48 -0300

    feat: import in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductModule } from './modules/product/product.module';
+import { RolesGuard } from './infra/providers/roles-guard.provider';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/auth_nestjs'), UsersModule, AuthModule, ProductModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{
+    provide: 'APP_GUARD',
+    useClass: RolesGuard
+  },AppService],
 })
 export class AppModule {}
