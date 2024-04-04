8cb88bcc2f8966d26b2b5266be683c52296e1972
Arthur <thurdelima@gmail.com>
2024-04-04 02:50:29 -0300

    feat: get and validate role user

--- src/infra/providers/roles-guard.provider.ts
+++ src/infra/providers/roles-guard.provider.ts
@@ -1,5 +1,7 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
 import { Observable } from "rxjs";
 import { Role, ROLES_KEY } from "src/decorators/roles.decorators";
 
@@ -8,17 +10,44 @@ import { Role, ROLES_KEY } from "src/decorators/roles.decorators";
 @Injectable()
 export class RolesGuard implements CanActivate {
 
-    constructor(private reflector: Reflector) {}
+    constructor(private reflector: Reflector, private jwtService: JwtService) { }
 
-    canActivate(context: ExecutionContext) {
-        
+    async canActivate(context: ExecutionContext) {
+
         const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [context.getHandler(), context.getClass])
 
         if (!requiredRoles) {
             return true;
         }
 
-        
+        const request = context.switchToHttp().getRequest();
+        const token = this.extractHeaderTokenFromHeader(request);
+
+        if (!token) throw new UnauthorizedException();
+
+        try {
+            const payload = await this.jwtService.verifyAsync(token, {
+                secret: 'cc9132d2ea1b006271fc2bed432a9da4'
+            })
+
+            request['user'] = payload;
+
+            return requiredRoles.some((role) => payload.user.roles.includes(role));
+
+
+        } catch (error) {
+            throw new UnauthorizedException();
+        }
+
+
     }
+
+    private extractHeaderTokenFromHeader(request: Request): string | undefined {
+        const [type, token] = request.headers.authorization?.split(' ') ?? [];
+
+        return type === 'Bearer' ? token : undefined;
+    }
+
+
 
 }
