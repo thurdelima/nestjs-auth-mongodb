b13d5ef6f5e097cdc76dfafd71ba54c6a447a36e
Arthur <thurdelima@gmail.com>
2024-04-05 02:25:15 -0300

    feat: create list product usecase

---
+++ src/modules/product/use-cases/list-product.usecase.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Product } from "../product.schema";
+import { Model } from "mongoose";
+
+
+
+
+Injectable()
+export class ListProductUseCase {
+
+    constructor(@InjectModel(Product.name) private productModel: Model<Product>) {}
+
+    async execute() {
+        return await this.productModel.find();
+    }
+
+}
